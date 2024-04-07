454b1ad741560290218dcda3b47cde5c7a8fb491
Casper <47537727+59thCasper@users.noreply.github.com>
2024-04-07 20:22:24 +0100

    Add files via upload

---
+++ noProtection/noProtection/Patches/SkipProtections.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using static noProtection.noProtection;
+
+namespace noProtection.Patches
+{
+    [HarmonyPatch(typeof(GridManager), "RegisterProtectedZone")]
+    public static class SkipRegisterProtectedZonePatch
+    {
+        [HarmonyPrefix] 
+        public static bool Prefix()
+        {
+
+            return false; 
+        }
+
+        [HarmonyPatch(typeof(VoxelManager), "RegisterProtectedVoxels")]
+        public static class SkipRegisterProtectedVoxelsPatch
+        {
+            [HarmonyPrefix] 
+            public static bool Prefix()
+            {
+                return false; 
+            }
+        }
+
+        [HarmonyPatch(typeof(CommonMachineInfo), "canErase", MethodType.Getter)]
+        public static class AlwaysCanErasePatch
+        {
+            [HarmonyPostfix] 
+            public static void Postfix(ref bool __result)
+            {
+                __result = true; 
+            }
+        }
+    }
+}
---
+++ noProtection/noProtection/noProtection.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace noProtection
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class noProtection : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.casper.noProtection";
+        private const string PluginName = "noProtection";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+
+        private void Awake()
+        {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+        }
+    }
+}
